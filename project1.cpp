b3d608bce05bcaa0adb56b0b8b7104aa9f89ed18
asahisatou <88087478+asahisatou@users.noreply.github.com>
2022-04-30 05:02:35 +0900

    komitto

---
+++ project1.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+
+int main() {
+ cv::Mat img;
+
+ img = cv::imread("/usr/local/pictures/chibany043.jpg");
+ cv::Mat gray;
+ cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+ cv::imshow("title", gray);
+ cv::waitKey(0);
+
+ return 0;
+}
